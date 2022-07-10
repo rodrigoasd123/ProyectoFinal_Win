b5ba268827a5261c1bec92360d2a1ecce016ef7c
FrankoRamirez <105893686+FrankoRamirez@users.noreply.github.com>
2022-07-10 18:22:27 -0500

    Revisión clase hija nuevo
    by @FrankoRamirez

--- src/informe_final_win/ClienteNuevo.java
+++ src/informe_final_win/ClienteNuevo.java
@@ -1,21 +1,12 @@
 
 package informe_final_win;
     
-    /*Clase Hija 2:
-      Si tienen atributos propios se inicializan.
-      Hacer el codigo de los metodos abstractos heredados de la clase padre.
-      Implementar metodos adicionales si se necesitan hacer operaciones (pueden ser del tipo funcion o procedimiento).
-      Para usar los atributos de la clase usar el getNombreAtributo() ya que estos son variables privadas.
-    */
-
 public class ClienteNuevo extends Cliente{
 
     public ClienteNuevo(String Nombres, String Apellidos, String dni, String distrito, String direccion, String tipocliente, 
             String tipoPlan) {
         super(Nombres, Apellidos, dni, distrito, direccion, tipocliente, tipoPlan);
     }
-    
-    //Puse los metodos para que no salga error, tu trabajo es editarlos    
     
     @Override 
     public double CalcularMontoInstalacion(){ 
