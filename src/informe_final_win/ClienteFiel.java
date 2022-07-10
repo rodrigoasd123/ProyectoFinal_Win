91496e038e3d0c108c2a2b535480d852252e039e
HP <HP@192.168.1.21>
2022-07-09 23:19:51 -0500

    Agregué los métodos heredados y los métodos especiales de la clase cliente fiel By:@Rodrigo

--- src/informe_final_win/ClienteFiel.java
+++ src/informe_final_win/ClienteFiel.java
@@ -9,18 +9,48 @@
     */
     
     public class ClienteFiel extends Cliente{
+
+    public ClienteFiel(String Nombres, String Apellidos, String dni, String distrito, String direccion, String tipocliente) {
+        super(Nombres, Apellidos, dni, distrito, direccion, tipocliente);
+    }
     
         
         
     //Puse los metodos para que no salga error, tu trabajo es editarlos    
-    @Override
-    public double CalcularMontoInstalacion() {
-       return 1.0;
-    }
+  
+public double CalcularDescuentoInstalacion(){
+return 120*0.3;
+}
 
-    @Override
-    public double CalcularMontoTotal() {
-       return 2.0;
-    }
+@Override 
+public double CalcularMontoInstalacion(){ 
+return 120-CalcularDescuentoInstalacion();
+}
+
+
+public double CalcularDescuentoPlan(){
+
+if(TipoPlan.equalsIgnoreCase("100 mbps"))
+return 79*0.95;
+
+if(TipoPlan.equalsIgnoraCase("200 mbps"))
+return 99*0.9;
+
+if(TipoPlan.equalsIgnoraCase("600 mbps"))
+return 169*0.85;
+
+if(TipoPlan.equalsIgnoraCase("600 mbps Plus"))
+return 259*0.80;
+
+if(TipoPlan.equalsIgnoraCase("1000 mbps"))
+return 459*0.75;
+
+return 0;
+}
+
+@Override
+public double CalcularMontoTotal(){
+return CalcularMontoInstalacion()+CalcularDescuentoPlan();
+}
             
     }
